ce52d348a39023ea1bb5aaa062e43cf83ce0fd7d
ZackXevious <zacharyalankirchens@gmail.com>
2025-02-24 15:53:48 -0600

    Uploaded files

---
+++ Assets/UdonScripts/DestroyOverTime.cs
@@ -0,0 +1,21 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class DestroyOverTime : UdonSharpBehaviour
+{
+    public float DeathDelay = 3;
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate() {
+        if (DeathDelay > 0) {
+            DeathDelay -= Time.deltaTime;
+        } else {
+            Destroy(this.gameObject);
+        }
+    }
+}
