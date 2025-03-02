eeb04f3fbc4966d231cfb4335ba8517dd0bca896
ZackXevious <zacharyalankirchens@gmail.com>
2025-03-02 16:29:38 -0600

    Continued work.
    Began work on adding hitboxes
    Began work on player Hud,
    experimented with texture work on maps,
    installed textmeshpro,
    added weapon magazines and ammo limits, began work on reloading weapons

---
+++ Assets/Scripts/GameControlling/GameControllerScript.cs
@@ -0,0 +1,35 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class GameControllerScript : UdonSharpBehaviour
+{
+    [SerializeField] GameObject test;
+    GameObject localHitbox;
+    private VRCPlayerApi localPlayer;
+    [SerializeField] GameObject playerHud; //This item contains the player's hud, if at all possible. Maybe also an extra hitbox for headshots?
+    void Start()
+    {
+        localPlayer = Networking.LocalPlayer;
+        localHitbox = Instantiate(test,localPlayer.GetPosition(), localPlayer.GetRotation());
+    }
+    public override void OnPlayerJoined(VRCPlayerApi player) {
+        if (player.IsUserInVR()) {
+            Debug.Log("Joined player is in VRC");
+        } else {
+            Debug.Log("Joined player is on PC");
+        }
+        
+    }
+
+    public override void PostLateUpdate() {
+        base.PostLateUpdate();
+        localHitbox.transform.position = localPlayer.GetPosition();
+        localHitbox.transform.rotation = localPlayer.GetRotation();
+        VRCPlayerApi.TrackingData headData = localPlayer.GetTrackingData(VRCPlayerApi.TrackingDataType.Head);
+        this.playerHud.transform.position = headData.position;
+        this.playerHud.transform.rotation = headData.rotation;
+    }
+}
--- Assets/Scripts/Weapons/GunScript.cs
+++ Assets/Scripts/Weapons/GunScript.cs
@@ -17,21 +17,46 @@ public class GunScript : UdonSharpBehaviour
     bool wantsToFire;
     float currTimer;
     [SerializeField] float timeBetweenBullets = .2f;
+    [SerializeField] int magSize = 12;
+    [SerializeField] int totalAllowedAmmo= 72;
+    int currMag;
+    int currAmmo;
 
     [Header("Sound Effects")]
     [SerializeField] AudioSource SoundSource;
     [SerializeField] AudioClip GunFireSound;
+    [SerializeField] AudioClip GunEmptySound;
     void Start()
     {
+        this.currAmmo = this.magSize * 3;
+        this.reloadWeapon();
         
+    }
+    public override void OnPickup() {
+        base.OnPickup();
+        //Get the player who picked this up
+        //Enable grip and reload interacts
+        //adjust location on screen for if player is on PC or in VR.
+        //Apply GUI stuff for player
     }
 
     public override void OnPickupUseDown() {
-        if (singleShot) {
-            SendCustomNetworkEvent(NetworkEventTarget.All, nameof(fireWeapon));
+        if (currMag>0) {
+            if (singleShot) {
+                SendCustomNetworkEvent(NetworkEventTarget.All, nameof(fireWeapon));
+            } else {
+                wantsToFire = true;
+            }
         } else {
-            wantsToFire = true;
+            //Play gunshot sound
+            if (this.SoundSource && this.GunEmptySound) {
+                //Change pitch
+                SoundSource.pitch = 1;
+                // Play Sound
+                SoundSource.PlayOneShot(this.GunEmptySound);
+            }
         }
+        
         
     }
     public override void OnPickupUseUp() {
@@ -45,27 +70,43 @@ public class GunScript : UdonSharpBehaviour
         if (wantsToFire && !singleShot) {
             SendCustomNetworkEvent(NetworkEventTarget.All,nameof(fireWeapon));
         }
+        if (Input.GetKey(KeyCode.R)) {
+            this.reloadWeapon();
+        }
     }
 
     public void fireWeapon() {
         if (currTimer != 0) { // Can't fire gun due to timer delay
             return;
         } else {
-            if (bulletSpawn && bulletPrefab) {
+            if (bulletSpawn && bulletPrefab && this.currMag>0) {
                 GameObject bulletInstance = Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
                 Rigidbody rb = bulletInstance.GetComponent<Rigidbody>();
                 if (rb) {
                     rb.AddForce(this.transform.forward * bulletSpeed, ForceMode.Impulse);
                 }
                 currTimer = timeBetweenBullets;
+
+                //Play gunshot sound
+                if (this.SoundSource && this.GunFireSound) {
+                    //Change pitch
+                    SoundSource.pitch = 1 + Random.Range(-.05f, .05f);
+                    // Play Sound
+                    SoundSource.PlayOneShot(this.GunFireSound);
+                }
+                this.currMag -= 1;
             }
-            if (this.SoundSource && this.GunFireSound) {
-                //Change pitch
-                SoundSource.pitch = 1 + Random.Range(-.05f, .05f);
-                // Play Sound
-                SoundSource.PlayOneShot(this.GunFireSound);
-            }
+            
         }
+    }
+
+    public void reloadWeapon() {
+        if (this.currAmmo == 0 || this.currMag == this.magSize) {
+            return ;
+        }
+        int ammoDifference = this.magSize - this.currMag;
+        this.currMag = this.magSize;
+
     }
     
 }
